86717ca61f406fc07b9b747c4adc94796fd68b78
Bts0 <brian.saunder@gmail.com>
2022-10-18 00:01:07 -0500

    Added support for Hibernate persistence. Moved persistence material to ProductController.java.

--- Product.java
+++ Product.java
@@ -66,38 +66,4 @@ public class Product {
         in_stock = is;
     }
 
-    public static void add(EntityManager em, UserTransaction ut, UUID uuid, String name, float price, int quantity) {
-        ut.begin();
-        Product product = new Product(uuid, name, price, quantity);
-        em.persist(product);
-        ut.commit();
-    }
-
-    public static List<Product> retrieveAll(EntityManager em) {
-        Query query = em.createQuery("SELECT p FROM Product p", Product.class);
-        List<Product> products = query.getResultList();
-        return products;
-    }
-
-    public static void update(EntityManager em, UserTransaction ut, UUID uuid, String name, float price, int quantity) throws Exception {
-        ut.begin();
-        Product product = em.find(Product.class, uuid);
-        if (!name.equals(product.getName())) product.setName(name);
-        if (price != product.getPrice()) product.setPrice(price);
-        if (quantity != product.getQuantity_in_stock()) product.setQuantity_in_stock(quantity);
-    }
-
-    public static void destroy(EntitityManager em, UserTransaction ut, UUID uuid) throws Exception {
-        ut.begin();
-        Product product = em.find(Product.class, uuid);
-        em.remove(product);
-        ut.commit();
-    }
-
-    public static void clearData(EntityManager em, UserTrasaction ut) throws Exception {
-        ut.begin();
-        Query deleteStatement = em.createQuery("DELETE FROM Product");
-        deleteStatement.executeUpdate();
-        ut.commit();
-    }
 }
